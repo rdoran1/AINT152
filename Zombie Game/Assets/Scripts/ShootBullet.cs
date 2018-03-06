6f234ad950412572044aaba27dfa3028555420f3
rdoran1 <33032112+rdoran1@users.noreply.github.com>
2018-03-06 11:25:37 +0000

    Workshop 2 - Zombie Shooter

---
+++ Zombie Game/Assets/Scripts/ShootBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float fireTime = 0.5f;
+
+    private bool isFiring = false;
+
+    void setFiring()
+    {
+        isFiring = false;
+    }
+
+    void Fire()
+    {
+        isFiring = true;
+        Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        if(GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            {
+                Fire();
+            }
+        }   
+    }
+}
